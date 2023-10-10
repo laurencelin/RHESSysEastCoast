47b2919eb53a9fce4fea87162844c4e3a5e4b134
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2023-10-10 11:39:53 -0400

    added inundation variables

--- output/output_patch.c
+++ output/output_patch.c
@@ -138,7 +138,7 @@ void	output_patch(
     
 
     
-	check = fprintf(outfile,"%d %d %d %d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+	check = fprintf(outfile,"%d %d %d %d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
                     
 					current_date.year, current_date.month, current_date.day, //1,2,3,
 					patch[0].ID, //4
@@ -171,7 +171,10 @@ void	output_patch(
                     top30cm_storage * 1000.0,
                     top30cm_potential_sat * 1000.0,
                     top60cm_storage * 1000.0,
-                    top60cm_potential_sat * 1000.0
+                    top60cm_potential_sat * 1000.0,
+		// Added additional variables for external coastal inundation, by Borstlap H, Oct 8, 2023
+		    ex_inundation_depth, 
+		    ex_inundation_dur, 
                     );
 
 	if (check <= 0) {
